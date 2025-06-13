9142cf3c3740d32ad22e093ba4e66fb470eab5f1
SarkyGit <40629195+SarkyGit@users.noreply.github.com>
2025-06-13 20:41:29 +0100

    Add Jinja2 base template and root route

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
@@ -6,3 +6,10 @@ app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 templates = Jinja2Templates(directory="app/templates")
+
+
+@app.get("/")
+async def read_root(request: Request):
+    """Render the base template with a test message."""
+    context = {"request": request, "message": "Hello, CES"}
+    return templates.TemplateResponse("base.html", context)
