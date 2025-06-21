2263a8ad7f7575e7c75187501f10283b2bb911dc
SarkyGit <40629195+SarkyGit@users.noreply.github.com>
2025-06-21 19:11:27 +0100

    Fix script path resolution

--- alembic/env.py
+++ alembic/env.py
@@ -1,4 +1,5 @@
-import os
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__) + '/../'))
 from dotenv import load_dotenv
 load_dotenv()
 from alembic import context
--- seed_tunables.py
+++ seed_tunables.py
@@ -1,3 +1,5 @@
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__) + '/../'))
 from core.utils.db_session import SessionLocal
 from core.models.models import SystemTunable
 import subprocess
